6921d5294188a3a8d84ab73cc83b4fe1391e08b5
TalhaBajwa41 <bajwaitx41@gmail.com>
2025-08-22 01:19:14 -0700

    push

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,8 +5,8 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      < CarHubHeader/>
-      < CarHubFooter/>
+      {/* < CarHubHeader/>
+      < CarHubFooter/> */}
     </div>
   )
 }
